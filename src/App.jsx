6f0f3362b8aec5e90af48d9982e74b66f756f836
design-park <audrey123012@gmail.com>
2025-04-05 10:12:35 +0200

    learned componet basics

--- src/App.jsx
+++ src/App.jsx
@@ -47,8 +47,19 @@ function App() {
         <h3>{postName[2]}</h3>
         <p>날짜</p>
       </div>
+
+      <Modal></Modal>
     </div>
   )
 }
 
+function Modal() {
+  return (
+    <div className = "modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+}
 export default App
