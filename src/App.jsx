b22c57df5d598edd8eaf055cf3bf9b022d0e1e1b
design-park <105970517+design-park@users.noreply.github.com>
2025-04-06 09:13:28 +0200

    change modal title based on clicked post my solution

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   let [postName, setPostName] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [likes, setLikes] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [clickedIndex, setClickedIndex] = useState(0);
 
   return (
     <div className = "App">
@@ -14,30 +15,13 @@ function App() {
         <h4>블로그임</h4>
       </div>
 
-      <button onClick = { () => {
-        let copy = [...postName];
-        copy[0] = '여자 코트 추천';
-        setPostName(copy);
-      } }>첫 글 제목 바꾸기</button>
-
-      <button onClick = { () => {
-        let copy = [...postName];
-        copy.shift();
-        copy.unshift('성공')
-        setPostName(copy);
-      } }>첫 글 제목 바꾸기</button>
-
-      <button onClick = { () => {
-        let copy = [...postName];
-        copy.sort();
-        setPostName(copy);
-      } }>가나다 정렬</button>
-
       {
         postName.map(function(a, i) {
           return (
             <div className = "list" key = {i}>
-              <h4 onClick = { () => {setModal(!modal)}}> 
+              <h4 onClick = { () => {
+                setClickedIndex(i);
+                setModal(!modal)}}> 
                 {postName[i]} 
                 <span onClick = { () => { 
                   let copy = [...likes];
@@ -52,7 +36,7 @@ function App() {
       }
 
       {
-        modal == true ? <Modal postName={postName} setPostName={setPostName}></Modal> : null
+        modal == true ? <Modal postName={postName} setPostName={setPostName} clickedIndex={clickedIndex}></Modal> : null
       }
     </div>
   )
@@ -61,12 +45,10 @@ function App() {
 function Modal(props) {
   return (
     <div className = "modal">
-      <h4>{ props.postName[0] }</h4>
+      <h4>{ props.postName[props.clickedIndex] }</h4>
       <p>날짜</p>
       <p>상세내용</p>
-      <button onClick = { () => {
-        props.setPostName(['여자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
-       }}>글 수정</button>
+      <button>글 수정</button>
     </div>
   )
 }
