9fcc8187680a5a5cadb7b5c63f0be9bb2d57b54c
design-park <105970517+design-park@users.noreply.github.com>
2025-04-06 12:02:18 +0200

    fixed modal hide/show error

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  let [postName, setPostName] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [postTitle, setpostTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [likes, setLikes] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
-  let [clickedTitle, setclickedTitle] = useState(0);
+  let [clickedTitleIndex, setclickedTitleIndex] = useState(0);
 
   return (
     <div className = "App">
@@ -16,13 +16,18 @@ function App() {
       </div>
 
       {
-        postName.map(function(a, i) {
+        postTitle.map(function(a, i) {
           return (
             <div className = "list" key = {i}>
-              <h4 onClick = { () => {
-                setclickedTitle(i);
-                setModal(!modal)}}> 
-                {postName[i]} 
+              <h4 onClick = {() => {
+                if (clickedTitleIndex == i) {  // if we click on the one that was already clicked
+                  setModal(!modal);
+                } else {
+                  setclickedTitleIndex(i);
+                  setModal(true);
+                }
+              }}> 
+                {postTitle[i]} 
                 <span onClick = { (e) => { 
                   e.stopPropagation(); // 부모 요소의 onClick 이벤트가 발생하지 않도록 함
                   let copy = [...likes];
@@ -37,29 +42,30 @@ function App() {
       }
 
       {
-        modal == true ? <Modal postName={postName} clickedTitle={clickedTitle}></Modal> : null
+        modal == true ? <Modal postTitle={postTitle} clickedTitleIndex={clickedTitleIndex}></Modal> : null
       }
     </div>
   )
 }
-/*
+
 function Modal(props) {
   return (
     <div className = "modal">
-      <h4>{ props.postName[props.clickedTitle] }</h4>
+      <h4>{ props.postTitle[props.clickedTitleIndex] }</h4>
       <p>날짜</p>
       <p>상세내용</p>
       <button>글 수정</button>
     </div>
   )
 }
-*/
 
+
+/*
 function Modal(props) {
   let [clicked, setClicked] = useState(false);
   return (
     <div className = "modal">
-      <h4>{ clicked ? "Clicked" : props.postName[props.clickedTitle] }</h4>
+      <h4>{ clicked ? "여자 코트 추천" : props.postTitle[props.clickedTitleIndex] }</h4>
       <p>날짜</p>
       <p>상세내용</p>
       <button onClick = { () => {
@@ -68,5 +74,6 @@ function Modal(props) {
     </div>
   )
 }
+*/
 
 export default App
